8998c4d7a0929871e54b56c038a6da2e359847ae
chorom-ham <chorom.m.ham@gmail.com>
2021-05-19 16:24:11 +0900

    🍱 chore: Change menu icon

--- src/assets/icons/menu.tsx
+++ src/assets/icons/menu.tsx
@@ -3,14 +3,18 @@ import { IconProps } from "./icons";
 export default function MenuIcon({ style, fill }: IconProps) {
   return (
     <svg
-      fill={fill}
-      style={style}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 50 50"
+      style={style}
+      fill="none"
+      stroke={fill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
     >
-      <path d="M8.667,15h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,15,8.667,15z" />
-      <path d="M8.667,37h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,37,8.667,37z" />
-      <path d="M8.667,26h30c0.552,0,1-0.447,1-1s-0.448-1-1-1h-30c-0.552,0-1,0.447-1,1S8.114,26,8.667,26z" />
+      <path d="M3 12L21 12"></path>
+      <path d="M3 6L21 6"></path>
+      <path d="M3 18L21 18"></path>
     </svg>
   );
 }
