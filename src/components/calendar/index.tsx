630c43618fd34fb1dc72f2d14242e389ce751466
chorom-ham <chorom.m.ham@gmail.com>
2021-05-19 20:02:48 +0900

    ✨ feat: Add calendar

---
+++ src/components/calendar/index.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+import { LIME } from "@colors";
+
+export default function CalendarComponents() {
+  return (
+    <Wrapper>
+      <TitleWrapper>
+        <Title>📆 Check the calendar 📆</Title>
+      </TitleWrapper>
+      <Calendar />
+    </Wrapper>
+  );
+}
+
+const TitleWrapper = styled.div`
+  background-color: ${LIME};
+  border-radius: 1rem;
+  margin: 2rem 0;
+  padding: 0.5rem 1rem;
+`;
+
+const Title = styled.h2`
+  font-size: 1.4rem;
+  font-weight: 500;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
