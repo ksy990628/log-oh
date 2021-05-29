b2b94e76999be59e8e0800fb593ca670191f2f3d
chorom-ham <chorom.m.ham@gmail.com>
2021-05-30 03:05:26 +0900

    ✨ feat: Add priority radio group

---
+++ src/components/create/priority-radiogroup.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export default function PriorityRadiogroup() {
+  const OPTIONS = ["1", "2", "3", "4", "5"];
+
+  const RadioItem = (value) => (
+    <RadioWrapper key={value}>
+      <RadioInput type="radio" value={value} id={value} />
+      <RadioLabel htmlFor={value}>{value}</RadioLabel>
+    </RadioWrapper>
+  );
+
+  return (
+    <InputGroup>
+      <Label htmlFor="priority">Priority</Label>
+      <RadioGroupWrapper>
+        {OPTIONS.map((value) => RadioItem(value))}
+      </RadioGroupWrapper>
+    </InputGroup>
+  );
+}
+
+const InputGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2rem;
+`;
+
+const Label = styled.label`
+  font-size: 1.6rem;
+  font-weight: 500;
+`;
+
+const RadioGroupWrapper = styled.div`
+  width: 70%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const RadioWrapper = styled.div`
+  display: flex;
+  align-items: baseline;
+`;
+
+const RadioInput = styled.input``;
+
+const RadioLabel = styled.label`
+  font-size: 1.5rem;
+  margin-left: 0.4rem;
+`;
