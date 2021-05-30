649ac3fc58b73b95ec7ceec52d405e903ac519c2
chorom-ham <chorom.m.ham@gmail.com>
2021-05-30 17:07:30 +0900

    ✨ feat: Add todo list

---
+++ src/components/home/todo-list.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+import { TodoItem as TodoItemType } from "src/types/todo-item";
+import TodoItem from "./todo-item";
+
+function ToDoList() {
+  const [list, setList] = useState([]);
+  const [reload, setReload] = useState(0);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("toDoList") || "[]");
+    setList(data);
+  }, [reload]);
+
+  const renderItems =
+    list.length > 0
+      ? list.map((item: TodoItemType, index: number) => (
+          <TodoItem
+            key={index + item.task}
+            index={index}
+            {...item}
+            reload={reload}
+            setReload={setReload}
+          ></TodoItem>
+        ))
+      : "Register new to-do!";
+
+  return <Wrapper>{renderItems}</Wrapper>;
+}
+
+export default ToDoList;
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 2rem;
+`;
